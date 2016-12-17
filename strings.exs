5f9a9b53e9f21c4f4c12950745ffbfa45ac73231
Jelani Thompson <jelani.thompson@outlook.com>
2016-12-17 16:19:07 -0500

    String methods

--- strings.exs
+++ strings.exs
@@ -6,4 +6,8 @@ IO.puts "Hello, #{name}"
 # Concatenation
 IO.puts "Hello, " <> name
 
+# The string module also contains a plethora of helpful functions
+IO.puts String.length("Rick_and_Morty")
+IO.puts String.upcase("meow!")
+
 # The IO module is used to write to standard input/output
