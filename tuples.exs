99171d861c633b0654f66a5df9f9682b2e517124
Jelani Thompson <jelani.thompson@outlook.com>
2016-12-27 20:48:02 -0500

    Tuples example

---
+++ tuples.exs
@@ -0,0 +1,6 @@
+# Tuples can hold any value like lists
+list = {"Hello", 72, :neutron}
+IO.puts tuple_size(list)
+
+# Tuples are meant to be more static and unchanging
+# Lists are more dynamic and faster to update
