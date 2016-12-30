0bb9fd455808942249cc0eebeaedcdabc20aaab3
Jelani Thompson <jelani.thompson@outlook.com>
2016-12-29 23:46:44 -0500

    Installing dependencies with deps.get

--- example/mix.exs
+++ example/mix.exs
@@ -27,6 +27,9 @@ defmodule Example.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+   [{:phoenix, "~> 1.1 or ~> 1.2"},
+   {:phoenix_html, "~> 2.3"},
+   {:cowboy, "~> 1.0", only: [:dev, :test]},
+   {:slime, "~> 0.14"}]
   end
 end
